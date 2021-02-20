c6b5fab13f166a234987147b46d1a9aef35b63fb
JannatJameel <j_annat@outlook.com>
2021-02-20 22:05:12 +0300

    minor change

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -11,7 +11,7 @@ const HomePage = () => {
         <div>
             <Welcome/>
             <Instructions/>
-            <RecentRecipes/>
+            {/* <RecentRecipes/> */}
         </div>
         
     )
